bd648ad4660a71623d993c95da35a9756700f5d3
bharatNakka <nbarath2009@gmail.com>
2017-02-18 17:25:29 +0530

    initial commit

---
+++ src/snippet/Snippet.java
@@ -0,0 +1,7 @@
+package snippet;
+
+public class Snippet {
+	svn import -m "C:\softwares\konakart" --force-log http://localhost:80/svn/konakart
+	svn import "project path" "to under URL" -m "message"
+}
+
